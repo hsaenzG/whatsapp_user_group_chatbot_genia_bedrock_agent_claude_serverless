7136052f099fb60173d41dd5a24de80da35fe853
Jorge Figueroa <jorgefigueroa1897@gmail.com>
2024-08-23 13:20:07 -0600

    Update on get faqs

--- private-assistant/lambdas/code/get_faqs/lambda_function.py
+++ private-assistant/lambdas/code/get_faqs/lambda_function.py
@@ -1,20 +1,22 @@
 import json
 import boto3
 from botocore.exceptions import ClientError
+import os
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('faqs_table')
+table = dynamodb.Table(os.environ.get('faqs_table'))
 
 def lambda_handler(event, context):
     print(json.dumps(event, indent=2))
 
+    parameters = event.get('parameters')
     api_path = event.get('apiPath')
-    country_event = event.get('CountryEvent')
-    question = event.get('Question')
+    id_event = next((param['value'] for param in parameters if param['name'] == 'id_event'), None)
+    print(id_event)
     response = {}
 
     if api_path == '/faqs':
-        events_info = get_faq(country_event, question)
+        events_info = get_faqs(id_event)
         print(events_info)
         response = events_info
 
@@ -38,12 +40,9 @@ def lambda_handler(event, context):
     print(result)
     return result
 
-def get_faq(country_event, question):
-    if not country_event or not question:
-        return {
-            "statusCode": 400,
-            "body": "CountryEvent y Question are required."
-        }
+def get_faqs(id_event):
+    if not id_event:
+        return { 'error': 'IdEvent is required.' }
 
     try:
         response = table.get_item(
